49e521f88e8873f306bfe51e95a7ebd4439d5ea1
Dat <jintoga123@yahoo.com>
2015-11-04 17:58:52 +0600

    2nd

--- AILab2/app/src/main/java/com/example/dat/ailab2/MainActivity.java
+++ AILab2/app/src/main/java/com/example/dat/ailab2/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.dat.ailab2;
 
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.dat.ailab2.Model.Figure;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -23,8 +30,16 @@ public class MainActivity extends AppCompatActivity {
             , R.id.cb31, R.id.cb32, R.id.cb33, R.id.cb34, R.id.cb35, R.id.cb36})
     List<CheckBox> checkBoxes;
 
+    List<Figure> listOfFigures;
+    int Q;
+    int Z;
+    double[][] matrixK;      //значение потенц ф-й обуч выборки
+    double[][] matrixL;      //обуч-е к-ты (лямбда)
+    String[] ch_class;
     @InjectView(R.id.buttonCheck)
     Button buttonCheck;
+    @InjectView(R.id.textViewResult)
+    TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +60,201 @@ public class MainActivity extends AppCompatActivity {
                 checked += checkBoxes.indexOf(checkBox) + " ";
             }
         }
-        Toast.makeText(this, checked, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, checked, Toast.LENGTH_SHORT).show();
+
+        if (initialize()) {
+            checkResult();
+        }
+
+        //clearStuff();
+    }
+
+    private void clearStuff() {
+        for (CheckBox checkBox : checkBoxes) {
+            checkBox.setChecked(false);
+        }
+    }
+
+    private void checkResult() {
+        boolean[] vec = getUserVec();
+        Figure userObr = new Figure(vec, -1);
+
+        double[] pot = new double[Q];
+
+        for (int j = 0; j < Q; j++) {
+            pot[j] = getPot(userObr, j);      // j - текущий класс
+        }
+
+        double maxValue = getMaxDouble(pot);
+        int m = toList(pot).indexOf(maxValue);     ///класс получился
+
+       /* Toast.makeText(this, "Это символ " + ch_class[m]
+                + "\n\nПотенциалы:\nКласс " + ch_class[0] + " = " + pot[0]
+                + "\nКласс " + ch_class[1] + " = " + pot[1]
+                + "\nКласс " + ch_class[2] + " = " + pot[2]
+                , Toast.LENGTH_LONG).show();*/
+        textViewResult.setText("Это символ " + ch_class[m]
+                        + "\n\nПотенциалы:\nКласс " + ch_class[0] + " = " + pot[0]
+                        + "\nКласс " + ch_class[1] + " = " + pot[1]
+                        + "\nКласс " + ch_class[2] + " = " + pot[2]
+        );
+
+    }
+
+    private boolean[] getUserVec() {
+        boolean[] vec = new boolean[36];
+
+        for (int i = 0; i < checkBoxes.size(); i++) {
+            if (checkBoxes.get(i).isChecked()) {
+                vec[i] = true;
+            }
+        }
+
+        return vec;
+    }
+
+    boolean doneInitializing = false;
+
+    private boolean initialize() {
+        getTeacher();       //подгружаем выборку
+        getMatrixK();       //получаем матрицу потенциалов
+        getMatrixL();       //получаем матрицу обучающих к-тов лямбда Q*Z. заполняем 0 и 1
+
+        teaching();
+        doneInitializing = true;
+        return doneInitializing;
+    }
+
+
+    private double getPot(Figure o, int j) {
+        double K = 0;
+        for (int z = 0; z < listOfFigures.size(); z++) {
+            K += matrixL[j][z] * listOfFigures.get(z).getK(o);
+        }
+        return K;
+    }
+
+    private boolean[] getVec(String vecString) {
+        boolean[] vec = new boolean[vecString.length()];
+
+        for (int i = 0; i < vecString.length(); i++) {
+            if (vecString.charAt(i) == '0') {
+                vec[i] = false;
+            } else vec[i] = true;
+        }
+
+        return vec;
+    }
+
+    private double getPotTeaching(int i, int j) //i-текущий ветор, j - текущий класс
+    {
+        double K = 0;
+        for (int z = 0; z < Z; z++) {
+            K += matrixL[j][z] * matrixK[i][z];
+        }
+        return K;
+    }
+
+    private void getMatrixK() {
+        matrixK = new double[Z][Z];
+        for (int i = 0; i < Z; i++) {
+            for (int j = 0; j < Z; j++) {
+                matrixK[i][j] = listOfFigures.get(i).getK(listOfFigures.get(j));
+            }
+        }
+    }
+
+    private void getMatrixL() {
+        Random rand = new Random();
+        matrixL = new double[Q][Z];
+        for (int i = 0; i < Q; i++) {
+            for (int j = 0; j < Z; j++) {
+                matrixL[i][j] = rand.nextInt(2);
+            }
+        }
+        Log.d("matrixL", matrixL.toString());
+    }
+
+    private void getTeacher() {
+        listOfFigures = new ArrayList<>();
+        TypedArray typedArray = getResources().obtainTypedArray(R.array.figures_list);
+        int n = typedArray.length();
+        String[][] array_classes = new String[n][];
+        for (int i = 0; i < typedArray.length(); i++) {
+            int resourceID = typedArray.getResourceId(i, 0);
+            if (resourceID > 0) {
+                array_classes[i] = getResources().getStringArray(resourceID);
+
+                Figure figure = new Figure();
+                figure.setName(array_classes[i][0]);
+                for (int j = 1; j < array_classes[i].length; j++) {
+                    array_classes[i][j] = array_classes[i][j].replaceAll("\\s", "");
+                    boolean[] vec = getVec(array_classes[i][j]);
+                    figure.setVect(vec);
+                    listOfFigures.add(figure);
+                }
+
+            } else {
+                Log.e("Error", "Res ERROR");
+            }
+        }
+        Z = listOfFigures.size();
+        Q = 3;
+        ch_class = getClassNames();
+        Log.d("classes", listOfFigures.toString());
+        typedArray.recycle();
+    }
+
+    private void teaching() {
+        boolean flag = true;
+
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < Z; i++) {
+                Figure curO = listOfFigures.get(i);
+                double[] pot = new double[Q];
+
+                for (int j = 0; j < Q; j++) {
+                    pot[j] = getPotTeaching(i, j);      //i-текущий ветор, j - текущий класс
+                }
+
+                double maxValue = getMaxDouble(pot);
+                int m = toList(pot).indexOf(maxValue);
+                //int m = Arrays.asList(pot).indexOf(maxValue);     ///класс получился
+                int p = curO.getClassN();                        //класс, к-й должен быть
+
+                if (m != p) {
+                    matrixL[p][i] += 1;      //корректируем
+                    matrixL[m][i] -= 1;
+                    flag = true;
+                }
+
+
+            }
+
+        }
+    }
+
+    private List<Double> toList(double[] pot) {
+        List<Double> list = new ArrayList<>();
+        for (int i = 0; i < pot.length; i++) {
+            list.add(pot[i]);
+        }
+        return list;
+    }
+
+    private double getMaxDouble(double[] array) {
+        double max = Double.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    private String[] getClassNames() {
+        String[] names = getResources().getStringArray(R.array.class_names);
+        return names;
     }
 }
---
+++ AILab2/app/src/main/java/com/example/dat/ailab2/Model/Figure.java
@@ -0,0 +1,68 @@
+package com.example.dat.ailab2.Model;
+
+/**
+ * Created by DAT on 11/4/2015.
+ */
+public class Figure {
+
+    boolean[] vect;
+    String name;
+    int classN;
+
+    public Figure() {
+    }
+
+    public Figure(boolean[] vect, int classN) {
+
+        this.vect = vect;
+        this.classN = classN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        if (name.equals("E")) {
+            setClassN(0);
+        } else if (name.equals("Э")) {
+            setClassN(1);
+        } else if (name.equals("+")) {
+            setClassN(2);
+        }
+    }
+
+    public boolean[] getVect() {
+        return vect;
+    }
+
+    public void setVect(boolean[] vect) {
+        this.vect = vect;
+    }
+
+    public int getMes(Figure o) {
+        int res = 0;
+        for (int i = 0; i < vect.length; i++) {
+            if (vect[i] != o.vect[i])
+                res++;
+        }
+        return res;
+    }
+
+    public double getK(Figure o)        //Потенциал
+    {
+        int mes = getMes(o);
+        double K = 1.0D / (1 + mes * mes);               //можно добавить параметр!!!!!
+        return K;
+
+    }
+
+    public int getClassN() {
+        return classN;
+    }
+
+    public void setClassN(int classN) {
+        this.classN = classN;
+    }
+}
